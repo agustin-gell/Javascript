809d615efd5e5670a29ce2a6d9b82f656c98896a
agustin-gell <agustin.gell28@gmail.com>
2022-05-04 22:35:28 -0300

    .

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,14 @@
+let numero = Number(prompt ("Escriba un numero"));
+let numero2 = Number(prompt ("Escriba otro numero"));
 
-console.log("Hola Agustin");
+let suma = numero+numero2
+let mul = numero*numero2
 
-alert ("HOLA CHUPA PIJAS");
+alert ("La multiplicacion es:"+mul);
+
+console.log ("La suma es: "+suma)
+
+
+
+
+
