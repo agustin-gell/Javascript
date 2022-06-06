78ae3f5487712b05919ece51921d9e26fd253ddb
agustin-gell <agustin.gell28@gmail.com>
2022-06-05 23:53:31 -0300

    aCT

--- Sistema alumnos/main.js
+++ Sistema alumnos/main.js
@@ -1,128 +1,153 @@
 class Alumno {
-    constructor(id, nombre, apellido, numeroDeRegistro) {
+    constructor(id, nombre, apellido, numeroDeRegistro, prom) {
         this.id = id
         this.nombre = nombre
         this.apellido = apellido
         this.numeroDeRegistro = numeroDeRegistro
+        this.prom = prom
     }
 }
-
-class Promedio {
-    constructor(id, promedio) {
-        this.id = id
-        this.promedio = promedio
-    }
-}
-
 
 const alumnos = []
-const promedios = []
 
-/*/ CLASES /*/
+/*/ inputs /*/
 
-function inicializarAplicacion() {
-    const titulo = document.createElement("h1")
-    titulo.innerText = "Sistema de notas"
-    document.body.appendChild(titulo)
+const inputNombre = document.createElement("input")
+inputNombre.type = "text"
+inputNombre.id = "inputNombre"
+inputNombre.placeholder = "NOMBRE"
+document.body.appendChild(inputNombre)
+
+const inputApellido = document.createElement("input")
+inputApellido.type = "text"
+inputApellido.id = "inputApellido"
+inputApellido.placeholder = "APELLIDO"
+document.body.appendChild(inputApellido)
+
+const inputNumeroDeRegistro = document.createElement("input")
+inputNumeroDeRegistro.type = "text"
+inputNumeroDeRegistro.id = "inputNumeroDeRegistro"
+inputNumeroDeRegistro.placeholder = "NÚMERO DE REGISTRO"
+document.body.appendChild(inputNumeroDeRegistro)
+
+const inputNota1 = document.createElement("input")
+inputNota1.type = "number"
+inputNota1.id = "inputNota1"
+inputNota1.placeholder = "NOTA 1"
+document.body.appendChild(inputNota1)
+
+const inputNota2 = document.createElement("input")
+inputNota2.type = "number"
+inputNota2.id = "inputNota2"
+inputNota2.placeholder = "NOTA 2"
+document.body.appendChild(inputNota2)
+
+const inputNota3 = document.createElement("input")
+inputNota3.type = "number"
+inputNota3.id = "inputNota3"
+inputNota3.placeholder = "NOTA 3"
+document.body.appendChild(inputNota3)
+
+const promedio = document.createElement("input")
+promedio.type = "number"
+promedio.id = "promedio"
+promedio.placeholder = "PROMEDIO"
+document.body.appendChild(promedio)
+
+/*/ inputs /*/
+
+/*/ botones /*/
+
+const botonCalucular = document.createElement("button")
+botonCalucular.innerHTML = "CALCULAR PROMEDIO"
+document.body.appendChild(botonCalucular)
+
+const botonAgregar = document.createElement("button")
+botonAgregar.innerHTML = "AGREGAR ALUMNO"
+document.body.appendChild(botonAgregar)
+
+const botonActualizar = document.createElement("button")
+botonActualizar.innerHTML = "ACTUALIZAR"
+document.body.appendChild(botonActualizar)
+
+
+/*/ botones /*/
+
+/*/ eventos /*/
+
+botonCalucular.addEventListener("click", () => {
+    calcularProm()
+})
+
+botonAgregar.addEventListener("click", () => {
+    agregarAlumnos()
+})
+
+botonActualizar.addEventListener("click", () => {
+    agregarAlumnos()
+})
+
+/*/ eventos /*/
+
+function calcularProm() {
+    let nota1 = Number(document.getElementById("inputNota1").value)
+    let nota2 = Number(document.getElementById("inputNota2").value)
+    let nota3 = Number(document.getElementById("inputNota3").value)
+    let total = (nota1 + nota2 + nota3) / 3
+
+    promedio.value = total.toFixed(2)
 }
 
-inicializarAplicacion()
-
-function crearMenu() {
-    const listaOpciones = ["Añadir alumno",
-        "Listar alumno",
-        "Buscar alumno"
-    ]
-
-    listaOpciones.forEach((opcion) => {
-
-        const btn = document.createElement("button");
-        btn.innerText = opcion;
-
-        switch (opcion) {
-            case "Añadir alumno": {
-                btn.addEventListener("click",
-                    () => {
-                        anadirAlumno();
-                    })
-                break;
-            }
-            case "Listar Usuarios": {
-                btn.addEventListener("click",
-                    () => {
-                        listarAlumno(alumnos);
-                    }
-                )
-                break;
-            }
-            case "Buscar Usuario": {
-                btn.addEventListener("click",
-                    () => {
-                        let encontrados = buscarUsuario();
-                        listarUsuarios(encontrados);
-                        console.table();
-                    })
-                break;
-            }
-        }
-        document.body.appendChild(btn);
-    })
-}
-
-function anadirAlumno() {
+function agregarAlumnos() {
     let id = 1;
     if (alumnos.length > 0) {
         id = alumnos[alumnos.length - 1].id + 1;
     }
 
-    let nombre = prompt("Ingrese el nombre del alumno");
-    let apellido = prompt("Ingrese el nombre del alumno");
-    let numeroDeRegistro = Number(prompt("Ingrese el número de registro del alumno"))
-    let alumno = new Alumno(id, nombre, apellido, numeroDeRegistro);
+    let nombre = document.getElementById("inputNombre").value
+    let apellido = document.getElementById("inputApellido").value
+    let numeroDeRegistro = document.getElementById("inputNumeroDeRegistro").value
+    let prom = promedio.value
+    let alumno = new Alumno(id, nombre, apellido, numeroDeRegistro, prom)
 
-    alumnos.push(alumno);
-    console.table(alumnos);
-    listarAlumnos(alumnos);
-}
-
-crearMenu()
+    alumnos.push(alumno)
+    console.log(alumnos)
 
 
-function listarAlumnos(miListaDeUsuarios) {
-    let miLista = document.querySelector("#listaAlumnos");
+    let miLista = document.querySelector("#listaAlumnos")
     if (!miLista) {
-        miLista = document.createElement("table");
-        miLista.setAttribute("id", "listaUsuarios");
+        miLista = document.createElement("table")
+        miLista.setAttribute("id", "listaAlumnos")
     }
-    miLista.innerHTML = "";
+    miLista.innerHTML = ""
 
-    const encabezado = document.createElement("tr");
+    const encabezado = document.createElement("tr")
 
-    const tdNombre = document.createElement("th");
-    tdNombre.innerHTML = "Nombre";
-    encabezado.appendChild(tdNombre);
+    const tdNombre = document.createElement("th")
+    tdNombre.innerHTML = "Nombre"
+    encabezado.appendChild(tdNombre)
 
-    const tdApellido = document.createElement("th");
-    tdApellido.innerHTML = "Apellido";
-    encabezado.appendChild(tdApellido);
+    const tdApellido = document.createElement("th")
+    tdApellido.innerHTML = "Apellido"
+    encabezado.appendChild(tdApellido)
 
-    const tdNumeroDeRegistro = document.createElement("th");
-    tdNumeroDeRegistro.innerHTML = "Número de registro";
-    encabezado.appendChild(tdNumeroDeRegistro);
+    const tdNumeroDeRegistro = document.createElement("th")
+    tdNumeroDeRegistro.innerHTML = "Número de registro"
+    encabezado.appendChild(tdNumeroDeRegistro)
 
-    const tdPromedio = document.createElement("th");
+    const tdPromedio = document.createElement("th")
     tdPromedio.innerHTML = "Promedio";
     encabezado.appendChild(tdPromedio);
 
     const tdAcciones = document.createElement("th");
     tdAcciones.innerHTML = "Acciones";
+    tdAcciones.id = "tdAcciones"
     encabezado.appendChild(tdAcciones);
 
     miLista.appendChild(encabezado)
 
-    miListaDeUsuarios.forEach((alumno, promedio) => {
-        const nodotr = document.createElement("tr")
-
+    alumnos.forEach((alumno) => {
+        const nodotr = document.createElement("tr");
         let nodotd = document.createElement("td");
         nodotd.innerHTML = `${alumno.nombre}`;
         nodotr.appendChild(nodotd)
@@ -136,52 +161,45 @@ function listarAlumnos(miListaDeUsuarios) {
         nodotr.appendChild(nodotd);
 
         nodotd = document.createElement("td");
-        nodotd.innerHTML = `${promedio.promedios}`;
-        nodotr.appendChild(nodotd);
+        nodotd.innerHTML = `${alumno.prom}`;
+        nodotr.appendChild(nodotd)
 
         nodotd = document.createElement("td");
-        nodotd.innerHTML = `<button id=${promedio.id}>Borrar</button> | Editar`
+        nodotd.innerHTML = `<button id=${alumno.id}>Borrar</button>`
 
         nodotr.appendChild(nodotd);
         miLista.appendChild(nodotr);
     });
 
     document.body.appendChild(miLista)
-    programarEventos();
+    programarEventos()
 }
 
 function programarEventos() {
     alumnos.forEach((element) => {
-        const btn = document.getElementById(element.id);
-        btn.addEventListener("click", () => agregarNota());
+        const botonBorrar = document.getElementById(element.id);
+        botonBorrar.addEventListener("click", () => eliminarUsuario());
     })
 }
 
-function agregarNota() {
-    let numeroDeRegistro = Number(prompt("INGRESE NUEVAMENTE EL NÚMERO DE REGISTRO DEL ALUMNO"))
+function eliminarUsuario() {
 
-    let numNotas = Number(prompt("INGRESE EL NUMERO DE NOTAS"))
-    let notasTotales = 0;
+    let id = Number(prompt("Ingrese el id del usuario que quiere eliminar"));
 
-    for (let i = 1; i <= numNotas; i++) {
-        let notaActual = Number(prompt("INGRESE LA NOTA " + (i)));
-        notasTotales += notaActual;
+    let encontrado = alumnos.find((alumno) => alumno.id === id);
 
-        if (notaActual > 10) {
-            alert("NOTA INVALIDA")
-        }
+    if (!encontrado) {
+        alert("USUARIO NO ENCONTRADO");
+    } else {
 
-        if (notaActual <= 0) {
-            alert("NOTA INVALIDA")
-        }
+        let index = alumnos.indexOf(encontrado);
+
+        alumnos.splice(index, 1);
+
+        console.log("Borrar usuario");
+        console.log(alumnos);
+
     }
 
-    let div = notasTotales / numNotas
-    let promedio = new Promedio(getId2(), div, numeroDeRegistro)
 
-    promedios.push(promedio)
-    console.log("PROMEDIOS")
-    console.table(promedios)
-
-    alert("EL PROMEDIO DEL ALUMNO ES " + div)
 }
